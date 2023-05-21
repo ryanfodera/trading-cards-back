593f5a6fac325e7878c16f9c100408793ab90c37
ryanfodera <ryanfodera@pursuit.org>
2023-05-21 18:17:00 -0400

    server updated

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
-const app = require("./app.js");
 require("dotenv").config();
+const app = require("./app.js");
+const sequelize = require("./config/db.js");
 
 const PORT = process.env.PORT || 3000;
 
-app.listen((PORT), () => {
-    console.log(`listing to port ${PORT}`)
+sequelize.sync({ force: false }).then(() => {
+    app.listen((PORT), () => {
+        console.log(`listing to port ${PORT}`);
+    });
+}).catch(err => {
+    console.error(err);
+    process.exit(1);
 });
